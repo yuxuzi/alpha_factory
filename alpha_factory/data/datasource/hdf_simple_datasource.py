b99ce165d3edbac50765bfa877f6ea2a982faca9
yuxuzi <yuxuzi@gmail.com>
2020-04-26 20:15:37 -0400

    set missing value for string type

--- alpha_factory/data/datasource/hdf_simple_datasource.py
+++ alpha_factory/data/datasource/hdf_simple_datasource.py
@@ -15,13 +15,6 @@ from zipline.pipeline.data.dataset import Column, DataSet
 from alpha_factory.data.datasource.mydataset import MyDataSet
 
 
-def _validate_input_column(self, column):
-    """Make sure a passed column is our column.
-    """
-    if column != self.column and column.unspecialize() != self.column:
-        raise ValueError("Can't load unknown column %s" % column)
-
-
 class HDFSimpleLoader(implements(PipelineLoader)):
     """
     pipline loader from HDF file
@@ -32,9 +25,8 @@ class HDFSimpleLoader(implements(PipelineLoader)):
         self.dates = dates.values
         self.assets = assets.values
 
-
     def load_adjusted_array(self, domain, columns, dates, sids, mask):
-        date_indexer =np.searchsorted(self.dates, dates)
+        date_indexer = np.searchsorted(self.dates, dates)
         assets_indexer = np.searchsorted(self.assets, sids)
 
         # Boolean arrays with True on matched entries
@@ -45,7 +37,7 @@ class HDFSimpleLoader(implements(PipelineLoader)):
         with pd.HDFStore(self.data_path) as store:
             for column in columns:
                 try:
-                    data = store["/data/"+column.name].values
+                    data = store["/data/" + column.name].values
                     data = data[np.ix_(date_indexer, assets_indexer)]
                     data[~mask] = column.missing_value
                 except KeyError:
@@ -63,11 +55,11 @@ class HDFSimpleDataSource:
     def __init__(self, data_path, dataset=None):
         self.data_path = data_path
         with pd.HDFStore(data_path) as store:
-            self.sids=store["index/sids"]
-            self.dates=store["index/dates"]
-            equities=store['equities'].assign(
-                        exchange='NYSE',
-                        country_code='US'
+            self.sids = store["index/sids"]
+            self.dates = store["index/dates"]
+            equities = store['equities'].assign(
+                exchange='NYSE',
+                country_code='US'
             )
 
         self.loader = HDFSimpleLoader(data_path, self.dates, self.sids)
@@ -75,6 +67,7 @@ class HDFSimpleDataSource:
         self.asset_finder = tmp_asset_finder(url='sqlite:///:memory:', equities=equities,
                                              exchanges=exchanges).__enter__()
 
+    @staticmethod
     def write_hdf(data, data_path):
         try:
             data.index = data.index.set_levels(data.index.levels[0].tz_localize('utc'), level=0)
@@ -85,8 +78,6 @@ class HDFSimpleDataSource:
         num_assets = len(symbols)
         sids = pd.Series(np.arange(num_assets))
         dates = pd.Series(data.index.levels[0].unique())
-        import pdb;
-        pdb.set_trace()
         dtypes = pd.Series(data.dtypes)
         starts, ends = [], []
         with pd.HDFStore(data_path, complevel=9, mode='w') as store:
@@ -110,10 +101,18 @@ class HDFSimpleDataSource:
             store.put('/dtypes', dtypes)
             store.put('/equities', equities)
 
-    def infer_dataset(self, fields=None):
-        with pd.HDFStore(self.data_path) as store:
+    @staticmethod
+    def infer_dataset(data_path, fields=None):
+
+        columns = {}
+        with pd.HDFStore(data_path) as store:
             dtypes = store['/dtypes']
-            columns = {k: Column(v) for k, v in dtypes.iteritems() if fields is None or k in fields}
+            for k, v in dtypes.iteritems():
+                if fields is None or k in fields:
+                    if v == 'O':
+                        columns[k] = Column(v, missing_value='NA')
+                    else:
+                        columns[k] = Column(v)
         return type("Dataset", (DataSet,), columns)
 
     def run_pipeline(self, *args, **kwargs):
@@ -124,25 +123,28 @@ class HDFSimpleDataSource:
         return engine.run_pipeline(*args, **kwargs)
 
 
-class RollingSumDifference(CustomFactor):
-    window_length = 3
-    inputs = [MyDataSet.open, MyDataSet.close]
-
-    def compute(self, today, assets, out, open, close):
-        out[:] = (open - close).sum(axis=0)
-
-
 if __name__ == '__main__':
-    def cal():
-        start_date, end_date = pd.Timestamp('2018-03-12'), pd.Timestamp('2018-03-27')
-        data_source = HDFSimpleDataSource("/home/yuxuzi/Data/mydataset", MyDataSet)
-        pipe = Pipeline(columns={'close': MyDataSet.close.latest,
-                                 'sumdiff': RollingSumDifference()
-                                 }, )
-        df = data_source.run_pipeline(pipe, start_date, end_date)
-        print(df)
+    data_path = "/home/yuxuzi/Data/mydataset2"
+
+    start_date, end_date = pd.Timestamp('2018-03-13'), pd.Timestamp('2018-03-27')
+    data_source = HDFSimpleDataSource(data_path)
+    dataset = HDFSimpleDataSource.infer_dataset(data_path)
+
+    if __name__ == '__main__':
+        universe=dataset.sector.latest.element_of(['A','B'])
 
 
-    t1 = timeit.default_timer()
-    timeit.timeit('cal()', number=10, setup="from __main__ import cal")
-    print("{} Seconds needed for single threaded execution".format(timeit.default_timer() - t1))
+    class RollingSumDifference(CustomFactor):
+        window_length = 3
+        inputs = [dataset.open, dataset.close]
+
+        def compute(self, today, assets, out, open, close):
+            out[:] = (open - close).sum(axis=0)
+
+
+    pipe = Pipeline(columns={'close': dataset.close.latest,
+                             'sumdiff': RollingSumDifference(),
+                             'sector': dataset.sector.latest
+                             },  screen=universe)
+    df = data_source.run_pipeline(pipe, start_date, end_date)
+    print(df)
